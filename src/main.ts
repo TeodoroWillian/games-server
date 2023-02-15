d17ec1c6fa4416b9786c2588de95dd06b8dc5733
Willian Teodoro <totawillian@gmail.com>
2023-02-15 15:31:03 -0400

    feat(user): implemente 'Controller' & 'Service'

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,7 @@ async function bootstrap() {
     .addTag("status")
     .addTag("Genre")
     .addTag('game')
+    .addTag('user')
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
