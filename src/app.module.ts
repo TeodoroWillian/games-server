38050428d20f75bce84ea0b98d0c7977463f1fef
Willian Teodoro <totawillian@gmail.com>
2023-02-15 13:58:20 -0400

    feat(game) generate resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GenreModule } from './genre/genre.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { GameModule } from './game/game.module';
 
 @Module({
-  imports: [GenreModule, PrismaModule],
+  imports: [GenreModule, PrismaModule, GameModule],
   controllers: [AppController],
   providers: [AppService],
 })
