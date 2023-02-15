9b3f2dace4ce82dde6a227b4ab9fb094e373499a
Willian Teodoro <totawillian@gmail.com>
2023-02-15 11:51:24 -0400

    feat(db) add model for 'Game' & 'User'

--- src/genre/genre.service.ts
+++ src/genre/genre.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  UnprocessableEntityException,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateGenreDto } from './dto/create-genre.dto';
 import { UpdateGenreDto } from './dto/update-genre.dto';
@@ -28,18 +32,28 @@ export class GenreService {
 
   create(dto: CreateGenreDto): Promise<Genre> {
     const data: Genre = { ...dto };
-    return this.prisma.genre.create({ data });
+    return this.prisma.genre.create({ data }).catch(this.handleError);
   }
 
   async update(id: string, dto: UpdateGenreDto): Promise<Genre> {
     await this.findByID(id);
 
     const data: Partial<Genre> = { ...dto };
-    return this.prisma.genre.update({ where: { id }, data });
+    return this.prisma.genre
+      .update({ where: { id }, data })
+      .catch(this.handleError);
   }
 
   async delete(id: string) {
     await this.findByID(id);
     await this.prisma.genre.delete({ where: { id } });
+  }
+
+  handleError(error: Error): undefined {
+    const errorLines = error.message?.split('/n');
+    const lastErrorLine = errorLines[errorLines.length - 1]?.trim();
+    throw new UnprocessableEntityException(
+      lastErrorLine || 'Algum erro ocorreu ao executar a operação',
+    );
   }
 }
